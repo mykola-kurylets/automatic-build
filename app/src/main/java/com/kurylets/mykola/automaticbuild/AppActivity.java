373de529f2e9ab789e30d0e335f43e3cf3b07799
mykola <mykola.kurylets@gmail.com>
2017-03-26 23:33:29 +0300

    init

---
+++ app/src/main/java/com/kurylets/mykola/automaticbuild/AppActivity.java
@@ -0,0 +1,15 @@
+package com.kurylets.mykola.automaticbuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+
+    // проба
+}
